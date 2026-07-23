23e1e8483420d549c0418eed297b7d7988231f98
AtharyAL_qofaf <arr26th@gmail.com>
2026-07-23 16:21:18 +0300

    Added CSS and improved project interface

--- students/urls.py
+++ students/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-app_name = 'students'
+app_name = "students"
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
 ]
