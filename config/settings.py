db1b4b3188af23a6a7491dfab2e0c5d0e1f7d71a
AtharyAL_qofaf <arr26th@gmail.com>
2026-08-14 17:42:32 +0300

    إضافة إعدادات البريد الإلكتروني

--- config/settings.py
+++ config/settings.py
@@ -122,3 +122,10 @@ STATICFILES_DIRS = [
     BASE_DIR / 'doctors' / 'static',
 ]
 AUTH_USER_MODEL = 'account.CustomUser'
+# Email Settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'atharyessam57@gmail.com'
+EMAIL_HOST_PASSWORD = 'wedecehmmnvjydey'
